a41e4a1576850e9b484feb89951917f58c7e9012
juan correa <juancorrea@MacBook-Pro-de-juan.local>
2023-02-20 18:34:09 -0500

    add service to connect weather api, also adding .env file inside .gitignore

---
+++ src/services/getWeatherCountry.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const getWeatherCountry = (country) => {
+    const URL_BASE = 'http://api.weatherstack.com/';
+    const URL_COMPLETE = URL_BASE + 'current?access_key=' + process.env.REACT_APP_WEATHER_API_KEY + '&query=' + country;
+
+        return axios.get(URL_COMPLETE)
+        .then(response => {
+        const {data} = response;
+        return data 
+        }).catch(err => console.warn('la comunicación con el servidor no ha sido posible', err));
+}
